7af5888bc1cd78c0b2d07a821b0e5f3f987c5b65
Trevor Hinkle <trevorhinkle@virginia.edu>
2016-09-11 18:55:37 -0400

    First commit
    first commit of personal site docs

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$('a.links').click(function (e){
+   e.preventDefault();
+   var div_id = $('a.links').index($(this))
+   $('.divs').fadeOut(750).eq(div_id).fadeIn(750);
+});
+
+//for portfolio page
+$('#rightArrow').click(function(){
+	
+	$(this).hide();
+	$('#leftArrow').show();
+
+	$('#page1').fadeOut(750);
+	$('#page2').fadeIn(750);
+
+	$('link3').addClass('active');
+
+	$('.mainInfo').animate({ scrollTop: $("#topOne").position().top });
+});
+
+$('#leftArrow').click(function(){
+	$(this).hide();
+	$('#rightArrow').show();
+
+	$('#page2').fadeOut(750);
+	$('#page1').fadeIn(750);
+
+	$('link3').addClass('active');
+});
+
+
